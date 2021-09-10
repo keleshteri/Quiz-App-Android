2c96510fe13f30a13d16e6992771fa94d210f99e
m.mehdi shaban keleshteri <mehdi.shaban.keleshteri@gmail.com>
2021-09-11 00:00:23 +0800

    Preparing the project's style and Android manifest file

--- app/src/main/java/com/keleshteri/quizandroidapp/MainActivity.kt
+++ app/src/main/java/com/keleshteri/quizandroidapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.keleshteri.quizandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.keleshteri.quizandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 }
